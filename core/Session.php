6b159a2427320d9ef5f07d0ffd48e9ce25606150
menej <68961385+menej@users.noreply.github.com>
2023-11-04 21:37:38 +0100

    Fix typo.

--- core/Session.php
+++ core/Session.php
@@ -32,7 +32,7 @@ class Session
 
         $params = session_get_cookie_params();
 
-        \setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
     }
 
     public static function login($user)
