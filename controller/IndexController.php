447b5f8318554821b23877975b23687a689e19ec
menej <68961385+menej@users.noreply.github.com>
2023-11-16 18:17:43 +0100

    refactor: Change a minor inconsistency

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -3,10 +3,9 @@
 namespace controller;
 
 require base_path('core/ViewHelper.php');
-require_once base_path("model/DBInit.php");
 
 use core\ViewHelper;
-use model\DBInit;
+
 
 class IndexController
 {
